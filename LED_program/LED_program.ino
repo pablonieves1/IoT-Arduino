f779edea27faa009fe95387eab40f520d099442b
pablonieves1 <pablonievesmo@gmail.com>
2025-03-28 22:01:49 +0100

    Finished the code to display on LEDs
    FInished developing the code to display the stock market price on a 8x64 led matrix with sliding text.

--- LED_program/LED_program.ino
+++ LED_program/LED_program.ino
@@ -2,7 +2,7 @@
 //Finnhub API (more promising with realtime data) 
 
 const char* FAPIKEY = ""; //Finnhub APIKEY for stocks real time date
-const char* AVAPIKEY = ""; //Alpha Vantage APIKEY for 
+const char* AVAPIKEY = "";//Alpha Vantage APIKEY for 
 //Finnhub query sample for Apple
 // "https://finnhub.io/api/v1/quote?symbol=AAPL&token=APIKEY"
 // ----> {"c":218.27,"d":4.17,"dp":1.9477,"h":218.84,"l":211.28,"o":211.56,"pc":214.1,"t":1742587200}
@@ -25,8 +25,14 @@ const char* AVAPIKEY = ""; //Alpha Vantage APIKEY for
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
+
+#define PIN 2
 
 const char* ssid = ""; //WIFI name (I think it has to have 2.4GHz enabled)
 const char* password = ""; //WIFI password
@@ -41,12 +47,28 @@ String symbols[numSymbols] = {"SPY", "XLK", "VT", "BINANCE:BTCUSDT", "BINANCE:ET
 const size_t capacity = JSON_OBJECT_SIZE(8); //Number of objetcts that will be extracted from the JSON
 StaticJsonDocument<capacity> doc;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//LED display for my setup. A strip of RGB leds with 64 columns and 8 rows.
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(64, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  //LED strip object definition
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(10);
+  matrix.setTextColor(colors[2]);
+
+  //WIFI setup
   WiFi.begin(ssid, password);
   Serial.print("Connecting");
 
@@ -60,23 +82,28 @@ void setup() {
   Serial.print("IP adress: ");
   Serial.println(WiFi.localIP());
 
-  lcd.init(); //Initialize LCD I2C screen for display
-  lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("INTERNET?");
-  delay(500);
-  lcd.setCursor(0,1);
-  lcd.print("CONECTADO!");
-  delay(4000);
-  lcd.clear();
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(10);
+  matrix.setTextColor(colors[2]);
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+  matrix.print(F("INTERNET?"));
+  matrix.show();
+
+  delay(1500);
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+  matrix.print(F("CONECTADO!"));
+  matrix.show();
+
+
 }
-
-
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+  matrix.setTextColor(colors[0]);
   if (WiFi.status() == WL_CONNECTED)
   {
     HTTPClient http;
@@ -105,6 +132,8 @@ void loop() {
 
         float currentPrice = doc["c"];
         String percentChange = doc["dp"];
+        percentChange.remove(percentChange.length() - 1, 1); //remove last character
+
 
         Serial.print("The  current price of " + symbol + " is: $");
         Serial.println(currentPrice);
@@ -113,7 +142,8 @@ void loop() {
         Serial.println("%");
 
         if (percentChange[0] != '-')
-        {percentChange = "+" + percentChange;}
+        {percentChange = "+" + percentChange;
+        matrix.setTextColor(colors[1]);}
         
         if (symbol == "BINANCE:BTCUSDT")
         {printsymbol = "BTC";}
@@ -129,20 +159,27 @@ void loop() {
         {printsymbol = "GLDM";}
         
         
-
-
-        lcd.clear();
-        lcd.setCursor(0,0);
         String screenPrint1 = printsymbol + ": $" + currentPrice;
-        lcd.print(screenPrint1);   //Printing the price of the symbol on the upper line
-
-        lcd.setCursor(0,1);
         String screenPrint2 = percentChange + "%";
-        lcd.print(screenPrint2);   //Printing the % variation of the symbol on the lower line
+
+        for (int x = 64; x > -135; x--){
+        matrix.fillScreen(0); 
+        matrix.setCursor(x, 0);   
+        matrix.print(screenPrint1 + " " + screenPrint2); 
+  
+
+        Serial.print(x);
+        matrix.show();
+        delay(40);
+        }    
 
 
-        delay(5000); //Delay between each display
       }
+
+
+      delay(100); //Delay between each display
+      
+      
 
     
     }
